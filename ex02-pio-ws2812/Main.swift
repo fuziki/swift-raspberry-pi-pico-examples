967f64d70a4fa53a50a954543aaf13763fd5b33c
fuziki <hymne.shannon.projects@gmail.com>
2024-08-18 00:28:54 +0900

    Add ex02-pio-ws2812

---
+++ ex02-pio-ws2812/Main.swift
@@ -0,0 +1,87 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors.
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+let LED_PIN: UInt32 = 17
+
+class WS2812 {
+    let ws2812_program_instructions: UnsafeMutablePointer<UInt16>
+    var ws2812_program: pio_program
+
+    init(ledPin: UInt32) {
+        ws2812_program_instructions = malloc(4 * MemoryLayout<UInt16>.size)!
+            .assumingMemoryBound(to: UInt16.self)
+        ws2812_program_instructions.advanced(by: 0).pointee = 0x6221
+        ws2812_program_instructions.advanced(by: 1).pointee = 0x1123
+        ws2812_program_instructions.advanced(by: 2).pointee = 0x1400
+        ws2812_program_instructions.advanced(by: 3).pointee = 0xa442
+
+        ws2812_program = pio_program(instructions: ws2812_program_instructions, length: 4, origin: -1)
+
+        let offset = pio_add_program(get_pio0(), &ws2812_program)
+        ws2812_program_init(get_pio0(), 0, offset, ledPin, 800000, false)
+    }
+
+    func set(color: UInt32) {
+        pio_sm_put_blocking(get_pio0(), 0, color)
+    }
+
+    // Removing this line will cause bugs.
+    func mayBeBug() {}
+}
+
+@main
+struct Main {
+    static func main() {
+        stdio_init_all()
+
+        let ws2812 = WS2812(ledPin: LED_PIN)
+
+        while true {
+            ws2812.set(color: 0xFF000000)
+            sleep_ms(1000)
+
+            // Green color (GRB format)
+            ws2812.set(color: 0x00FF0000)
+            sleep_ms(1000)
+
+            // Blue color (GRB format)
+            ws2812.set(color: 0x0000FF00)
+            sleep_ms(1000)
+        }
+    }
+}
+
+// https://github.com/apple/swift-playdate-examples/blob/749dd8f518429168d03e754764afb334a80b527d/Sources/Playdate/Playdate.swift#L21
+@_documentation(visibility: internal)
+@_cdecl("posix_memalign")
+public func posix_memalign(
+  _ memptr: UnsafeMutablePointer<UnsafeMutableRawPointer?>,
+  _ alignment: Int,
+  _ size: Int
+) -> CInt {
+  guard let allocation = malloc(Int(size + alignment - 1)) else {
+    #if hasFeature(Embedded)
+    fatalError()
+    #else
+    fatalError("Unable to handle memory request: Out of memory.")
+    #endif
+  }
+  let misalignment = Int(bitPattern: allocation) % alignment
+  #if hasFeature(Embedded)
+  precondition(misalignment == 0)
+  #else
+  precondition(
+    misalignment == 0,
+    "Unable to handle requests for over-aligned memory.")
+  #endif
+  memptr.pointee = allocation
+  return 0
+}
